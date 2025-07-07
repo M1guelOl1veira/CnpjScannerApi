cc4d76c205f11672349190f5090a01e6f8be531e
Miguel Oliveira <miguel.ao1086@gmail.com>
2025-07-07 13:08:17 -0300

    Uncommenting code

--- CnpjScanner.Api/Services/MultiLanguageAnalyzerService.cs
+++ CnpjScanner.Api/Services/MultiLanguageAnalyzerService.cs
@@ -21,27 +21,27 @@ public class MultiLanguageAnalyzerService
         if (!Directory.Exists(directoryPath))
             return result;
 
-        // var csharpResults = _csharp.AnalyzeCSharpFiles(directoryPath)
-        //     .Select(r => { r.Language = "C#"; return r; });
-        // result.AddRange(csharpResults);
+        var csharpResults = _csharp.AnalyzeCSharpFiles(directoryPath)
+            .Select(r => { r.Language = "C#"; return r; });
+        result.AddRange(csharpResults);
 
         var vbnetResults = _vbnet.AnalyzeDirectory(directoryPath)
             .Select(r => { r.Language = "VB.NET"; return r; });
         result.AddRange(vbnetResults);
 
-        // var tsResults = await _typescript.AnalyzeAsync(directoryPath);
-        // foreach (var tsMatch in tsResults)
-        // {
-        //     result.Add(new VariableMatch
-        //     {
-        //         FilePath = tsMatch.FilePath,
-        //         LineNumber = tsMatch.LineNumber,
-        //         Declaration = tsMatch.Declaration,
-        //         LooksLikeCnpj = tsMatch.LooksLikeCnpj,
-        //         Language = "TypeScript",
-        //         Type = "number"
-        //     });
-        // }
+        var tsResults = await _typescript.AnalyzeAsync(directoryPath);
+        foreach (var tsMatch in tsResults)
+        {
+            result.Add(new VariableMatch
+            {
+                FilePath = tsMatch.FilePath,
+                LineNumber = tsMatch.LineNumber,
+                Declaration = tsMatch.Declaration,
+                LooksLikeCnpj = tsMatch.LooksLikeCnpj,
+                Language = "TypeScript",
+                Type = "number"
+            });
+        }
 
         return result.OrderByDescending(x => x.LooksLikeCnpj).ToList();
     }
